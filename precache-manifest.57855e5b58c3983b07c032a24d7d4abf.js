eca8052937c729a3450eca6fa2191fd12b1c08a2
edmundohh <1edmundoh@gmail.com>
2019-01-04 00:40:28 -0800

    Updates

---
+++ precache-manifest.57855e5b58c3983b07c032a24d7d4abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8de2ad73919bed9eb19",
+    "url": "/static/js/main.a8de2ad7.chunk.js"
+  },
+  {
+    "revision": "46738a6dc0738ee0917e",
+    "url": "/static/js/1.46738a6d.chunk.js"
+  },
+  {
+    "revision": "a8de2ad73919bed9eb19",
+    "url": "/static/css/main.7f27154f.chunk.css"
+  },
+  {
+    "revision": "46738a6dc0738ee0917e",
+    "url": "/static/css/1.80f58afa.chunk.css"
+  },
+  {
+    "revision": "08a3af237454b43c55ed2ba126c50184",
+    "url": "/index.html"
+  }
+];
