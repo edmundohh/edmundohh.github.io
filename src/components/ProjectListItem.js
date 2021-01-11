35cd07540ec34dc12ee8cb91cd1edcf532a43c3f
edmundohh <1edmundoh@gmail.com>
2021-01-10 17:02:46 -0800

    Refactor projects section, add new project

---
+++ src/components/ProjectListItem.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import '../css/Projects.css';
+
+function ProjectListItem({ photo, title, subtitle, skills, description, ghLink, demoLink }) {
+  console.log(skills)
+  return (
+    <div className="project">
+      <div className="project-photo">
+        <img src={photo}></img>
+      </div>
+      <div className="profile-title">
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+      </div>
+      <ul className="skills">
+        {skills.map((skillitem) => (
+          <li>{skillitem}</li>
+        ))}
+      </ul>
+      <div className="project-description">
+        <p>{description}</p>
+      </div>
+      <div className="github-link">
+        <a href={ghLink}><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
+      </div>
+      { demoLink
+        ? <div className="demo-link">
+            <a href={demoLink}><i class="fa fa-laptop fa-4x" aria-hidden="true"></i></a>
+          </div>
+        : null
+      }
+  </div>
+  );
+}
+
+export default ProjectListItem;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
 import '../css/Projects.css';
-import ProjectPhoto3 from '../images/projects/incognitoelf.png';
-import ProjectPhoto2 from '../images/projects/ubcsublet.png';
-import ProjectPhoto1 from '../images/projects/boxblaster.png';
-import ProjectPhoto4 from '../images/projects/helpinghand.png';
+import ProjectListItem from './ProjectListItem';
+
+import IncognitoElfPhoto from '../images/projects/incognitoelf.png';
+import ubcSubletPhoto from '../images/projects/ubcsublet.png';
+import BoxBlasterPhoto from '../images/projects/boxblaster.png';
+import HelpingHandPhoto from '../images/projects/helpinghand.png';
+import TeePartyPhoto from '../images/projects/teeparty.png';
+
+const projects = [
+	{
+		photo: TeePartyPhoto,
+		title: "TeeParty",
+		skills: ["React Native", "Node.js", "Express.js", "MongoDB"],
+		description: "A mobile app that enables a friendly competition between you and your friends by logging your scores at your favourite golf course. Follow other players to keep up with their score logs and view them on the leaderboard.",
+		ghLink: "https://github.com/edmundohh/TeeParty",
+	},
+	{
+		photo: HelpingHandPhoto,
+		title: "Helping Hand",
+		subtitle: "1st Place at UBC Local Hack Day 2019",
+		skills: ["PostgreSQL", "Node.js", "React.js"],
+		description: "A web app allowing easy donations to the homeless by simply scanning a QR code printout acquired from a participating merchant.",
+		ghLink: "https://github.com/edmundohh/HelpingHand",
+		demoLink: "https://devpost.com/software/helpinghand-a4swt1"
+	},
+	{
+		photo: IncognitoElfPhoto,
+		title: "Incogito Elf",
+		skills: ["Node.js", "MongoDB"],
+		description: "A Secret Santa app for users to create and join groups to organize a gift exchange.",
+		ghLink: "https://github.com/edmundohh/IncognitoElf"
+	},
+	{
+		photo: ubcSubletPhoto,
+		title: "ubcSublet",
+		skills: ["Node.js", "React.js", "Oracle"],
+		description: "A UBC Sublet system allowing users to sign up, create posts, and make requests to posts in order to sublet an available room on campus.",
+		ghLink: "https://github.com/edmundohh/ubcSublet"
+	},
+	{
+		photo: BoxBlasterPhoto,
+		title: "Box Blaster",
+		skills: ["Unity", "C#"],
+		description: "A tower defense game, with the goal of defending your territory from box enemies by constructing Box Blaster Towers.",
+		ghLink: "https://github.com/edmundohh/Unity-BoxBlaster-Tower-Defense-Game"
+	}
+]
 
 class Projects extends Component {
+	renderItem() {
+		return projects.map((item) => {
+			return (
+				<ProjectListItem
+					photo={item.photo}
+					title={item.title}
+					subtitle={item.subtitle}
+					skills={item.skills}
+					description={item.description}
+					ghLink={item.ghLink}
+					demoLink={item.demoLink}
+				/>
+			);
+		});
+	}
+
 	render() {
 		return(
 			<div className="projects-section">
@@ -13,73 +72,10 @@ class Projects extends Component {
 					<h1>projects</h1>
 				</div>
 		    <ul className="projects-list">
-			<li><div className="project">
-						<div className="project-photo">
-							<img src={ ProjectPhoto4 }></img>
-						</div>
-						<div className="profile-title">
-							<h2>Helping Hand</h2>
-							<h3>1st Place at UBC Local Hack Day 2019</h3>
-						</div>
-						<ul className="skills">
-							<li> PostgreSQL </li>
-							<li> Node.js </li>
-							<li> React.js </li>
-						</ul>
-						<div className="project-description">
-							<p> 
-							A web app allowing easy donations to the homeless by simply scanning a QR code printout acquired from
-							a participating merchant.
-							</p>
-						</div>
-						<div className="github-link">
-							<a href="https://github.com/edmundohh/HelpingHand"><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
-						</div>
-						<div className="demo-link">
-							<a href="https://devpost.com/software/helpinghand-a4swt1"><i class="fa fa-laptop fa-4x" aria-hidden="true"></i></a>
-						</div>
-				</div></li>
-		    	<li><div className="project">
-						<div className="project-photo">
-							<img src={ ProjectPhoto3 }></img>
-						</div>
-						<div className="profile-title">
-							<h2>Incognito Elf</h2>
-						</div>
-						<ul className="skills">
-							<li> Node.js </li>
-							<li> MongoDB </li>
-						</ul>
-						<div className="project-description">
-							<p> 
-							A Secret Santa app for users to create and join groups to organize a gift exchange.
-							</p>
-						</div>
-						<div className="github-link">
-							<a href="https://github.com/edmundohh/IncognitoElf"><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
-						</div>
-				</div></li>
-				<li><div className="project">
-						<div className="project-photo">
-							<img src={ ProjectPhoto2 }></img>
-						</div>
-						<div className="profile-title">
-							<h2>ubcSublet</h2>
-						</div>
-						<ul className="skills">
-							<li> Node.js </li>
-							<li> React.js </li>
-							<li> Oracle </li>
-						</ul>
-						<div className="project-description">
-							<p> 
-							A UBC Sublet system allowing users to sign up, create posts, and make requests to posts in order to sublet an available room on campus. 
-							</p>
-						</div>
-						<div className="github-link">
-							<a href="https://github.com/edmundohh/ubcSublet"><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
-						</div>
-				</div></li>
+					<li>{this.renderItem()}</li>
+			{/*
+		    	
+				
 				<li><div className="project">
 						<div className="project-photo2">
 							<img src={ ProjectPhoto1 }></img>
@@ -99,7 +95,7 @@ class Projects extends Component {
 						<div className="github-link">
 							<a href="https://github.com/edmundohh/Unity-BoxBlaster-Tower-Defense-Game"><i class="fa fa-github fa-4x" aria-hidden="true"></i></a>
 						</div>
-				</div></li>
+				</div></li> */}
 			</ul>
 		</div>
 		);
